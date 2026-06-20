528a140b58587d0635387cd2d5531efa8baa9050
Mandowwow <setoferahian@gmail.com>
2026-06-21 01:26:03 +0300

    add basic game over state and restart func

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ image_path = os.path.join(base_dir, "Assets", "Sprites", "Sky1.jpg")
 image_goblin_path = os.path.join(base_dir, "Assets", "Sprites", "goblin.png")
 image_hero_path = os.path.join(base_dir, "Assets", "Sprites", "hero.png")
 font_path = os.path.join(base_dir, "Assets", "Fonts", "Pixeltype.ttf")
-
 test_font = pygame.font.Font(font_path, 65)
+
+game_active = True
 
 sky_surface = pygame.image.load(image_path).convert_alpha()
 
@@ -29,7 +30,7 @@ goblin_surface = pygame.transform.scale(goblin_surface, (70, 70))
 goblin_rect = goblin_surface.get_rect(midbottom = (1152, 620))
 
 hero_surface = pygame.image.load(image_hero_path).convert()
-hero_surface = pygame.transform.scale(hero_surface, (74,104))
+hero_surface = pygame.transform.scale(hero_surface, (74,104 ))
 hero_rect = hero_surface.get_rect(midbottom = (64, 620))
 hero_gravity = 0
 
@@ -39,52 +40,45 @@ while True:
             pygame.quit() 
             exit()
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if hero_rect.collidepoint(event.pos) and hero_rect.bottom >= 620: 
-                hero_gravity = -20
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if hero_rect.collidepoint(event.pos) and hero_rect.bottom >= 620: 
+                    hero_gravity = -20
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and hero_rect.bottom >= 620:
+                    hero_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                goblin_rect.left = 1152
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and hero_rect.bottom >= 620:
-                hero_gravity = -20
+    if game_active:
+        screen.blit(sky_surface, (0,0))
+        screen.blit(ground_surface, (0,620))
+        pygame.draw.rect(screen, 'Pink', score_rect)
+        pygame.draw.rect(screen, 'Pink', score_rect, 6)
+        #pygame.draw.line(screen, 'Gold', (0,0), pygame.mouse.get_pos(), 10)
+        screen.blit(score_surf, score_rect)
 
-        if event.type == pygame.KEYUP:
-            print('key up')
+        #PLAYER
+        hero_gravity += 1
+        hero_rect.y += hero_gravity
+        #hero_rect.x += 3
+        if hero_rect.bottom >= 620:
+            hero_rect.bottom = 620
+        screen.blit(hero_surface, hero_rect)
 
-    screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0,620))
-    pygame.draw.rect(screen, 'Pink', score_rect)
-    pygame.draw.rect(screen, 'Pink', score_rect, 6)
-    #pygame.draw.line(screen, 'Gold', (0,0), pygame.mouse.get_pos(), 10)
-    screen.blit(score_surf, score_rect)
+        screen.blit(goblin_surface, goblin_rect)
+        goblin_rect.right -= 4
+        if goblin_rect.left < -128: goblin_rect.left = 1280
 
-    #PLAYER
-    hero_gravity += 1
-    hero_rect.y += hero_gravity
-    #hero_rect.x += 3
-    if hero_rect.bottom >= 620:
-        hero_rect.bottom = 620
-    screen.blit(hero_surface, hero_rect)
+        #COLLISION
+        if goblin_rect.colliderect(hero_rect):
+            game_active = False
+    else:
+        screen.fill('Yellow')
 
-    screen.blit(goblin_surface, goblin_rect)
-    goblin_rect.right -= 4
-    if goblin_rect.left < -128: goblin_rect.left = 1280
-
-    #COLLISION
-    if goblin_rect.colliderect(hero_rect):
-        pygame.quit() 
-        exit()
-
-    #keys = pygame.key.get_pressed()
-    #if keys[pygame.K_SPACE]:
-    #    print('jump')
-
-    #if hero_rect.colliderect(goblin_rect):
-        #print('collision')
-    
-    #mouse_pos = pygame.mouse.get_pos()
-    #if hero_rect.collidepoint(mouse_pos):
-    #    print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
